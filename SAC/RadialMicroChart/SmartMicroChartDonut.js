e2d0b1d36071091fd091763a2edf7dc691d3b82f
FHasba1204 <47748179+FHasba1204@users.noreply.github.com>
2021-12-12 22:14:19 +0100

    Update SmartMicroChartDonut.js

--- SAC/RadialMicroChart/SmartMicroChartDonut.js
+++ SAC/RadialMicroChart/SmartMicroChartDonut.js
@@ -201,7 +201,7 @@
         	console.log("--First Time --");
 
         	let div0 = document.createElement('div');   
-        	div0.innerHTML = '<?xml version="1.0"?><script id="oView_' + widgetName + '" name="oView_' + widgetName + '" type="sapui5/xmlview"><mvc:View xmlns="sap.m" xmlns:mvc="sap.ui.core.mvc" controllerName="myView.Template"><RadialMicroChart size="L" percentage="' + percentage + '" press="onPress" class="sapUiSmallMargin"></RadialMicroChart></mvc:View></script>';
+        	div0.innerHTML = '<?xml version="1.0"?><script id="oView_' + widgetName + '" name="oView_' + widgetName + '" type="sapui5/xmlview"><mvc:View xmlns="sap.suite.ui.microchart" xmlns="sap.m" xmlns:mvc="sap.ui.core.mvc" controllerName="myView.Template"><RadialMicroChart size="L" percentage="' + percentage + '" press="onPress" class="sapUiSmallMargin"></RadialMicroChart></mvc:View></script>';
         	_shadowRoot.appendChild(div0);  
 
             let div1 = document.createElement('div');            
